e1ba1f79c83c9b0f5beea41819bb73dbe3df9575
sohankervin <sohankervin@gmail.com>
2018-03-09 05:33:03 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+"""
+this portion of the program will verify if this is a riffle shuffle or not by determining if the card is in either/ or deck. if so, 
+return True, if not, return False.
+"""
+# what you need to is verify if it's actually part of the single riffle 
+# measure against 2 split arrays of the original 52 elements of the deck
+
+
+import riffleShuffle1
+
+if in riffleShuffle1.binary() : 
+    return true
+
+
+
